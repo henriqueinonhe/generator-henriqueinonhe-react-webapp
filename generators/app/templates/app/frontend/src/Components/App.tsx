959f292b6e22f0be11538020a57308f795194011
Henrique Inonhe <henriqueinonhe@gmail.com>
2020-10-12 15:36:38 -0300

    Implemented boilerplate App component

---
+++ generators/app/templates/app/frontend/src/Components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() : JSX.Element
+{
+  return (
+    <>
+      Hello World!
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
