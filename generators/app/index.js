70de36ac113d3dbc696a6db42733c3eb22b32602
Henrique Inonhe <henriqueinonhe@gmail.com>
2020-12-31 12:24:00 -0300

    New version

--- generators/app/index.js
+++ generators/app/index.js
@@ -17,6 +17,61 @@ module.exports = class extends Generator {
   }
 
   install() {
-    this.spawnCommand("npm", ["install"], { cwd: "app"});
+    //Dependencies
+    this.npmInstall([
+      "react",
+      "react-dom",
+      "styled-components"
+    ], {
+      "dev": true
+    });
+
+    //Dev Dependencies
+    this.npmInstall([
+      //Webpack
+      "webpack",
+      "webpack-cli",
+      //Webpack Plugins
+      "webpack-node-externals",
+      "html-webpack-plugin",
+      "clean-webpack-plugin",
+      "fork-ts-checker-webpack-plugin",
+      "case-sensitive-paths-webpack-plugin",
+      "@nenado/watch-missing-node-modules-plugin",
+      //Webpack Loaders
+      "ts-loader",
+      "babel-loader",
+      "css-loader",
+      "style-loader",
+      //Typescript
+      "typescript",
+      "typedoc",
+      //Jest
+      "jest",
+      "ts-jest",
+      "jest-puppeteer",
+      "expect-puppeteer",
+      //Eslint
+      "eslint",
+      "eslint-plugin-react",
+      "@typescript-eslint/eslint-plugin",
+      "@typescript-esline/parser",
+      //Babel
+      "@babel/core",
+      "@babel/preset-env",
+      "@babel/preset-react",
+      //Types (for typescript)
+      "@types/jest",
+      "@types/jest-environment-puppeteer",
+      "@types/node",
+      "@types/puppeteer",
+      "@types/react",
+      "@types/react-dom",
+      "@types/styled-components",
+      //Other
+      "dotenv"
+    ], {
+      "save-dev": true
+    });
   }
 };
--- generators/app/templates/app/webpack.config.js
+++ generators/app/templates/app/webpack.config.js
@@ -1,69 +1,146 @@
 const path = require("path");
-const nodeExternals = require("webpack-node-externals");
-const LiveReloadPlugin = require('webpack-livereload-plugin');
+const webpack = require("webpack");
 
-const config = type => env => ({
-  mode: env.NODE_ENV,
-  entry: type === "frontend" ? "./frontend/src/Components/App.tsx" : "./backend/src/index.ts",
-  output: type === "frontend" ? {
-    path: path.resolve(__dirname, "./frontend/public"),
-    filename: "bundle.js"
-  } : 
-  {
-    path: path.resolve(__dirname, "./backend"),
-    filename: "index.js"
-  },
-  target: type === "frontend" ? "web" : "node",
-  externals: type === "frontend" ? [] : [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: "ts-loader",
-          options: {
-            //transpileOnly: true
-          }
-        },
-        include: /src/
-      },
-      {
-        test: /\.m?js$/,
-        include: [path.resolve(__dirname, "./backend/src"), path.resolve(__dirname, "./core/src"), path.resolve(__dirname, "./frontend/src")],
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-react"
-            ],
-            plugins: [
-              "babel-plugin-styled-components"
-            ],
-            cacheDirectory: true
-          }
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif|mp3)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "assets"
-            }
-          }
-        ]
+//Plugins
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const WatchMissingNodeModulesPlugin = require("@nenado/watch-missing-node-modules-plugin");
+
+const config = env => {
+  const srcPath = path.resolve(__dirname, "src");
+  
+  const typescriptLoaderRule = {
+    test: /\.(ts|tsx)$/,
+    include: [srcPath],
+    use: {
+      loader: "ts-loader",
+      options: {
+        //Type checking is handled by ForkTsChecker
+        transpileOnly: true,
+        //Build paralelization
+        happyPackMode: true
       }
-    ]
-  },
-  devtool: env.NODE_ENV === "development" ? "source-map" : "none",
-  resolve: {
-    extensions: [".ts", ".js", ".json", ".tsx"],
-    symlinks: false
-  },
-  plugins: [
-    new LiveReloadPlugin()
-  ] 
-});
+    },
+  };
+  
+  //React and styled components transpiling
+  const babelLoaderRule = {
+    test: /\.(mjs|js)$/,
+    include: [srcPath],
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          "@babel/preset-react"
+        ],
+        plugins: [
+          "babel-plugin-styled-components"
+        ],
+        cacheDirectory: true,
+        //Gzipping cache
+        compact: env.NODE_ENV === "production"
+      }
+    }
+  };
 
-module.exports = [config("frontend"), config("backend")];
+  //Importing CSS
+  const cssLoaderRule = {
+    test: /\.css$/i,
+    use: ["style-loader", "css-loader"]
+  };
+
+  //Importing assets
+  const fileLoaderRule = {
+    test: /\.(png|jpe?g|gif|mp3|svg|woff|woff2|eot|ttf|otf|mp4)$/i,
+    type: "asset/resource"
+  };
+
+  return {
+    mode: env.NODE_ENV,
+    //Stops compilation on the first error
+    bail: env.NODE_ENV === "production",
+    devtool: env.NODE_ENV === "development" ? "eval-cheap-module-source-map" : "source-map",
+    entry: {
+      index: "./src/index.tsx",
+    },
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: env.NODE_ENV === "development" ? "[name].js" : "[fullhash].[name].js",
+      pathinfo: false,
+      assetModuleFilename: "assets/[name].[ext]",
+      //So that every resource will be served as if the URL was "/" (client side routing)
+      publicPath: "/"
+    },
+    module: {
+      strictExportPresence: true,
+      rules: [
+        typescriptLoaderRule,
+        babelLoaderRule,
+        cssLoaderRule,
+        fileLoaderRule,
+        translationLoaderRule
+      ]
+    },
+    resolve: {
+      //Only tries to resolve modules with these extensions
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+      //Doesn't resolve symlinks to actual location (improves build performance)
+      symlinks: false,
+      //Allows us to reference files (when importing) without specifying 
+      //their extensions (.js, .ts, etc)
+      enforceExtension: false,
+      //Not sure what this does, so be aware (but should improve build performance)
+      cacheWithContext: false
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "NODE_ENV": env
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        async: env.NODE_ENV === "development",
+        typescript: {
+          checkSyntacticErrors: true,
+          tsconfig: "./tsconfig.json"
+        }
+      }),
+      new CaseSensitivePathsPlugin(),
+      new WatchMissingNodeModulesPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: "Title",
+        filename: "index.html",
+        meta: { viewport: "width=device-width, initial-scale=1, user-scalable=no" },
+        chunks: ["index"]
+      })
+    ],
+    optimization: {
+      minimize: env.NODE_ENV === "production",
+      mergeDuplicateChunks: true,
+      removeEmptyChunks: env.NODE_ENV === "production",
+      removeAvailableModules: env.NODE_ENV === "production",
+      splitChunks: {
+        chunks: "all",
+        //Test this
+        name: false
+      }
+    },
+    devServer: {
+      contentBase: "./public",
+      //Gzip compression for everything
+      compress: true,
+      liveReload: true,
+      //Opens browser automatically
+      open: true,
+      //Shows warnings/errors as overlay on the web page
+      overlay: true,
+      //Serves index.html to every route (client side routing)
+      historyApiFallback: true
+    },
+  };
+}
+
+
+
+module.exports = [config];
